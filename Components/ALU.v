1a0e75ff0c2d678b0eb3855b2252922d327803cd
Ahmed Ramy Said Hussein Ali ElNaggar <ahmedramysaidelnaggar@gmail.com>
2025-05-12 19:47:44 +0300

    Update ALU.v

--- Components/ALU.v
+++ Components/ALU.v
@@ -1,16 +1,21 @@
-module ALU (reg_one, reg_two, opcode, out);
-    input wire [18:0] reg_one, reg_two;
-    input wire [1:0] opcode;
-    output reg [18:0] out;
+module ALU (reg_one, reg_two, opcode, zero_flag, out);
+	input wire [18:0] reg_one, reg_two; // rs, rt
+	input wire [2:0] opcode;
+	output reg zero_flag;
+	output reg [18:0] out;
 
 	always @(*)
 		begin
 		case(opcode)
-			2'b00: out = reg_one + reg_two;
-			2'b01: out = reg_one - reg_two;
-			2'b10: out = reg_one & reg_two;
-			2'b11: out = reg_one | reg_two;
+			3'b000: out = reg_one + reg_two;
+			3'b001: out = reg_one - reg_two;
+			3'b010: out = reg_one & reg_two;
+			3'b011: out = reg_one | reg_two;
+			3'b100: out = ($signed(reg_one) < $signed(reg_two)) 
+								? 1 : 0;
 			default: out = 0;
 		endcase
-    end
+		zero_flag = (out == 0) ? 1'b1 : 1'b0;
+	end
+
 endmodule
