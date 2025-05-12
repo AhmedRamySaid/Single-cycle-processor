ac0ccecaac2f4344325dd02dd51d056de4fbb4b7
Ahmed Ramy Said Hussein Ali ElNaggar <ahmedramysaidelnaggar@gmail.com>
2025-05-12 19:49:06 +0300

    Update ROM.v

--- Components/ROM.v
+++ Components/ROM.v
@@ -6,11 +6,9 @@ module Instructions(
 	// 31 registers of 19 bits each containing an instruction
 	reg [18:0] instructionMemory [0:31];
 
+
 	initial begin
-		instructionMemory[0] = 19'b0; //Empty instruction used for initialization
-		instructionMemory[1] = 19'b0100_000_001_111_111_111;
-		instructionMemory[2] = 19'b0100_000_010_000_000_111;
-		instructionMemory[3] = 19'b0000_001_010_011_000_010;
+		$readmemb("output.mem", instructionMemory); 
 	end
 	
 	// Return instruction
